cf85ba57237583ad3f2d00c3539a8fb9dc6ed8b5
Daniela Cardozo <danielacar89@hotmail.com>
2024-10-27 12:02:16 -0300

    validacion email y campos

--- js/validacion.js
+++ js/validacion.js
@@ -1,25 +1,54 @@
+let all_fields = document.getElementsByTagName('input');
+
+function validateFields(){
+    for (let i = 0; i < all_fields.length; i++) {
+        if (emptyField(all_fields[i]) && !isPassword(all_fields[i])) {
+            console.log(all_fields[i]);
+            all_fields[i].classList.add('is-invalid');
+        } else {
+            all_fields[i].classList.remove('is-invalid');
+        }
+    }
+}
+
+function emptyField(field) {
+    return (field.value === '');
+}
+
+function isPassword(field) {
+    return (field.id === "password1" || field.id === "password2");
+}
+
 function validateEmail() {
     const email = document.getElementById('email');
     const validityState = email.validity;
     const emailFeedback = document.getElementById('emailFeedback');
 
-    console.log(email.checkValidity());
     if (validityState.valueMissing) {
-    emailFeedback.textContent = 'Debe ingresar un email';
-      email.classList.add('is-invalid');
+        emailFeedback.textContent = 'Debe ingresar un email';
+        email.classList.add('is-invalid');
     } else if (validityState.patternMismatch) {
-    emailFeedback.textContent = 'El email debe tener un formato válido';
-      email.classList.add('is-invalid');
-    } else  if(validityState.valid) {
+        emailFeedback.textContent = 'El email debe tener un formato válido';
+        email.classList.add('is-invalid');
+    } else if (validityState.valid) {
         email.classList.remove('is-invalid');
         email.classList.add('is-valid');
         emailFeedback.textContent = '';
     }
-  
-    email.reportValidity();
-  }
 
-  document.getElementById("submit").addEventListener("click", function(event) {
-    event.preventDefault(); // Evitar que el formulario se envíe inmediatamente
-    validateEmail(); // Llamar a la función de validación
+    email.reportValidity();
+}
+
+const form = document.getElementById("form");
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío inmediato del formulario
+    validateFields(); // Valida los campos genéricos
+    validateEmail();  // Valida específicamente el campo de correo electrónico
+
+    // Aquí puedes verificar si algún campo aún tiene la clase 'is-invalid' antes de enviar
+    const invalidFields = form.querySelectorAll('.is-invalid');
+    if (invalidFields.length === 0) {
+        form.submit(); // Solo envía el formulario si todas las validaciones son correctas
+    }
 });
